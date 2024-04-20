7f7a53cfbcf522ab1e1c01688a737aca16748fee
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2024-04-20 16:49:03 -0400

    Finger Labeling

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ while True:
         
         fingerUp = detector.fingersUp(lmList)
         print(fingerUp)
+        if fingerUp == [1,0,0,0,0]:
+            cv2.putText(frame, "Thumb", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+        elif fingerUp == [0,1,0,0,0]:
+            cv2.putText(frame, "Index", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+        elif fingerUp == [0,0,1,0,0]:
+            cv2.putText(frame, "Middle", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+        elif fingerUp == [0,0,0,1,0]:
+            cv2.putText(frame, "Ring", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+        elif fingerUp == [0,0,0,0,1]:
+            cv2.putText(frame, "Pinky", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
     
     cv2.imshow("Frame", frame)
     
