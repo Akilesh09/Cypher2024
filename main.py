60ad41fc2c6f2290328336b808fd224306861aa0
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2024-04-20 16:29:34 -0400

    Finger Tracking

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-detector = HandDetector(detectionCon=.8,maxHands=1)
+detector = HandDetector(detectionCon=.8, maxHands=1)
 
 video = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video.read()
+    
+    if not ret:
+        print("Error: Failed to read frame from the video.")
+        break
+    
     hands, img = detector.findHands(frame)
-    cv2.imshow("Frame",frame)
+    
+    if hands:
+        lmList = hands[0]
+        
+        fingerUp = detector.fingersUp(lmList)
+        print(fingerUp)
+    
+    cv2.imshow("Frame", frame)
+    
     k = cv2.waitKey(1)
-    if k ==ord('q'):
+    if k == ord('q'):
         break
 
 video.release()
