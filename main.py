e94cd98c0e57c16cf2777730ae35969ba35c3beb
Akilesh Mahesh <akilesh.mahesh07@gmail.com>
2024-04-21 11:07:10 -0400

    Final

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-import math
+import pyautogui
 
 detector = HandDetector(detectionCon=.8, maxHands=1)
 
 video = cv2.VideoCapture(0)
+last_key_pressed = "None"
 
 while True:
     ret, frame = video.read()
@@ -18,27 +19,36 @@ while True:
     if hands:
         hand1 = hands[0]
         
-        fingerUp = detector.fingersUp(hand1)
-        centerPoint = hand1["center"]
-        print(fingerUp)
-        if fingerUp == [1,0,0,0,0]:
-            cv2.putText(frame, "Thumb", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
-        elif fingerUp == [0,1,0,0,0]:
-            cv2.putText(frame, "Index", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
-            #indexFinger = lmList[8:12]
-            #dx = indexFinger[0][0] - indexFinger[1][0]
-            #dy = indexFinger[0][1] - indexFinger[1][1]
-            #angle = math.atan2(dy, dx)
-            #angle = math.degrees(angle)
-            #cv2.putText(frame, f"Angle: {int(angle)}", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
-        elif fingerUp == [0,0,1,0,0]:
-            cv2.putText(frame, "Middle", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
-        elif fingerUp == [0,0,0,1,0]:
-            cv2.putText(frame, "Ring", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
-        elif fingerUp == [0,0,0,0,1]:
-            cv2.putText(frame, "Pinky", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
-        elif sum(fingerUp) >= 4:
-            cv2.putText(frame, "Open: " + str(centerPoint), (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+        fingers = detector.fingersUp(hand1)
+        print(fingers)
+        if sum(fingers) == 5:
+            cv2.putText(frame, "Palm Open", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+            if last_key_pressed != "Palm Open":
+                pyautogui.press('volume_up')
+                last_key_pressed = "Palm Open"
+        elif sum(fingers) == 0:
+            cv2.putText(frame, "Palm Closed", (20, 460), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255, 255, 255), 2)
+            if last_key_pressed != "Palm Closed":
+                pyautogui.press('volume_down')
+                last_key_pressed = "Palm Closed"
+        elif fingers == [0,1,0,0,0]:
+            if last_key_pressed != "Index":
+                pyautogui.press('j')
+                last_key_pressed = "Index"
+        elif fingers == [0,1,1,0,0]:
+            if last_key_pressed != "Index and Middle":
+                pyautogui.press('l')
+                last_key_pressed = "Index and Middle"
+        elif fingers == [0,1,1,1,0]:
+            if last_key_pressed != "Index, Middle, and Ring":
+                pyautogui.press('space')
+                last_key_pressed = "Index, Middle, and Ring"
+        elif fingers == [0,1,1,1,1]:
+            if last_key_pressed != "All Fingers":
+                pyautogui.press('volume_mute')
+                last_key_pressed = "All Fingers"
+    
+    cv2.putText(frame, f"Last Key Pressed: {last_key_pressed}", (20, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2)
     
     cv2.imshow("Frame", frame)
     
